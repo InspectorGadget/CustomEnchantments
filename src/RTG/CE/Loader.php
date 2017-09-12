d928410697d580d4d52c2ae5fd60075cacfd65f2
RTG <igadget28@gmail.com>
2017-09-12 21:10:02 +0800

    Update

---
+++ src/RTG/CE/Loader.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RTG
+ * Date: 12/9/2017
+ * Time: 9:06 PM
+ */
+
+namespace RTG\CE;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Loader extends PluginBase implements Listener {
+
+    public function onEnable() {
+
+    }
+
+    public function onDisable() {
+        
+    }
+
+}
